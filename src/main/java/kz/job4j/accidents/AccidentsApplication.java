43979f4edcd429d0ed481b80df02ea16b976a01e
aidos-dev <aidos@paygate.kz>
2023-12-22 15:38:51 +0600

    init

---
+++ src/main/java/kz/job4j/accidents/AccidentsApplication.java
@@ -0,0 +1,13 @@
+package kz.job4j.accidents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccidentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccidentsApplication.class, args);
+	}
+
+}
