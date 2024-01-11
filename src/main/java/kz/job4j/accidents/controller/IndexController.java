6839293591e496229114b24007c2064095149866
aidos-dev <aidos@paygate.kz>
2024-01-11 09:50:55 +0600

    add Model, add navbar with text, add header.html

--- src/main/java/kz/job4j/accidents/controller/IndexController.java
+++ src/main/java/kz/job4j/accidents/controller/IndexController.java
@@ -1,13 +1,15 @@
 package kz.job4j.accidents.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
-    @GetMapping
-    public String getDefault() {
+    @GetMapping("/")
+    public String getDefault(Model model) {
+        model.addAttribute("user", "Petr Arsentev");
         return "index";
     }
 }
