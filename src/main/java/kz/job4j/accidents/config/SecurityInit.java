9952c2484a7ff30c7cd3250a2f4e413643844915
urmoonfriend <konyspay.aidos@gmail.com>
2024-01-21 14:54:38 +0600

    add security

---
+++ src/main/java/kz/job4j/accidents/config/SecurityInit.java
@@ -0,0 +1,6 @@
+package kz.job4j.accidents.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
