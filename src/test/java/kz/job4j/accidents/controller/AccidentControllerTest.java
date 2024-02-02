ef0506bdfd46b2ac417cebbc50eff30ef19ba869
aidos-dev <aidos@paygate.kz>
2024-02-02 10:49:51 +0600

    try to fix tests

--- src/test/java/kz/job4j/accidents/controller/AccidentControllerTest.java
+++ src/test/java/kz/job4j/accidents/controller/AccidentControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,6 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@ActiveProfiles("test")
 public class AccidentControllerTest {
 
     @Autowired
