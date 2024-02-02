7e3ea8d43f00e5465276827bb8ca61b30d7baddd
aidos-dev <aidos@paygate.kz>
2024-02-02 16:19:54 +0600

    try to fix tests

--- src/test/java/kz/job4j/accidents/controller/LoginControllerTest.java
+++ src/test/java/kz/job4j/accidents/controller/LoginControllerTest.java
@@ -1,14 +1,13 @@
 package kz.job4j.accidents.controller;
 
-import kz.job4j.accidents.AccidentsApplication;
+import kz.job4j.accidents.EnvConfigTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -17,9 +16,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
-@TestPropertySource(value = "application-test.properties")
-public class LoginControllerTest {
+@Transactional
+public class LoginControllerTest extends EnvConfigTest {
 
     @Autowired
     private MockMvc mockMvc;
