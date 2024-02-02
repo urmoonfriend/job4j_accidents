bb6c217dbb45fc7036655c1f3bbce20d000bc1cf
aidos-dev <aidos@paygate.kz>
2024-02-02 11:43:47 +0600

    try to fix tests

--- src/test/java/kz/job4j/accidents/controller/LoginControllerTest.java
+++ src/test/java/kz/job4j/accidents/controller/LoginControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -17,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@TestPropertySource
 public class LoginControllerTest {
 
     @Autowired
